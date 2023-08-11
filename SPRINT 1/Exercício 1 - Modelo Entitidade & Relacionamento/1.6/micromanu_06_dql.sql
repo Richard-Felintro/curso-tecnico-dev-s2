d4a22907632ac6807d91db12cc5c58b6a8d6ce5e
Richard Felintro <richardfelintro06@gmail.com>
2023-08-11 16:48:56 -0300

    Exercícios opcionais de MER e SQL concluídos.

--- SPRINT 1/Exercício 1 - Modelo Entitidade & Relacionamento/1.6/micromanu_06_dql.sql
+++ SPRINT 1/Exercício 1 - Modelo Entitidade & Relacionamento/1.6/micromanu_06_dql.sql
@@ -1,13 +1,39 @@
 -- DQL - Data Query Language
 USE Exercicio_1_6
 
--- Listar todos os pedidos dos clientes
+/* Listar todos os pedidos dos clientes */
 SELECT
+Cliente.Nome AS 'Cliente',
 Tipo.Nome AS 'Tipo de Eletrônico',
-
+Especialista.Nome AS 'Especialista Responsável',
+Empresa.Nome AS 'Empresa Responsável'
 
 FROM
-Tipo
+Conserto
+LEFT JOIN Eletronico ON Conserto.IdEletronico = Eletronico.IdEletronico
+LEFT JOIN Tipo ON Eletronico.IdTipo = Tipo.IdTipo
+LEFT JOIN Cliente ON Eletronico.IdCliente = Cliente.IdCliente
+LEFT JOIN Especialista ON Especialista.IdEspecialista = Conserto.IdEspecialista
+LEFT JOIN Empresa ON Empresa.IdEmpresa = Especialista.IdEmpresa
 
 
-- listar todos os pedidos de um determinado cliente, mostrando quais foram os colaboradores que executaram o serviço, qual foi o tipo de conserto, qual item foi consertado e o nome deste cliente
+/* Listar todos os pedidos de um determinado cliente, 
+mostrando quais foram os colaboradores que executaram o serviço, 
+qual foi o tipo de conserto, qual item foi consertado e o nome deste cliente */
+
+SELECT
+Cliente.Nome AS 'Cliente',
+Tipo.Nome AS 'Tipo de Eletrônico',
+Especialista.Nome AS 'Especialista Responsável',
+Empresa.Nome AS 'Empresa Responsável'
+
+FROM
+Conserto
+LEFT JOIN Eletronico ON Conserto.IdEletronico = Eletronico.IdEletronico
+LEFT JOIN Tipo ON Eletronico.IdTipo = Tipo.IdTipo
+LEFT JOIN Cliente ON Eletronico.IdCliente = Cliente.IdCliente
+LEFT JOIN Especialista ON Especialista.IdEspecialista = Conserto.IdEspecialista
+LEFT JOIN Empresa ON Empresa.IdEmpresa = Especialista.IdEmpresa
+
+WHERE
+Cliente.IdCliente = 1
